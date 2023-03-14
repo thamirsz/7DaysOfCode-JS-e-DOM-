71c36b5a21ce3e5ddbb0cbc956aead7967357d54
thamires <thamiresamaral2015@gmail.com>
2023-03-14 13:59:02 -0300

    Dias 4 e 5

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 function checkValidity(){
 let nameField = document.querySelector('#name');
+let birthdate = document.querySelector('#birthdate');
 // Adiciona um ouvinte de evento de entrada ao campo de entrada de nome
 nameField.addEventListener('input', () => {
   nameField.setCustomValidity(""); // remove qualquer mensagem de erro personalizada existente
@@ -23,67 +24,79 @@ nameField.addEventListener('change', () => {
 
 
 // função para exibir os dados salvos em uma tabela
-  function exibirDados() {
-    const tbody = document.querySelector('.pessoas-js tbody');
-    tbody.innerHTML = '';
 
-    // recupera os dados salvos localmente e exibe-os em uma tabela
-    const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
-    pessoas.forEach((pessoa, index) => {
-      const tr = document.createElement('tr');
-      const tdNome = document.createElement('td');
-      const tdAniversario = document.createElement('td');
-      const tdExcluir = document.createElement('td'); // nova coluna para o botão de excluir
-      
-      tdNome.textContent = pessoa.nome;
-      
-    const data = new Date(pessoa.aniversario);
-    // formata a data para o formato "DD/MM/AAAA"
-    tdAniversario.textContent = data.toLocaleDateString('pt-BR');
+function exibirDados() {
+  const tbody = document.querySelector('.pessoas-js tbody');
+  tbody.innerHTML = '';
 
-      // cria um botão de "Excluir" para cada linha
-      const btnExcluir = document.createElement('button');
-      btnExcluir.textContent = 'Excluir';
-      btnExcluir.addEventListener('click', () => {
-        // remove o item selecionado da tabela e dos dados salvos no armazenamento local
-        pessoas.splice(index, 1);
-        localStorage.setItem('pessoas', JSON.stringify(pessoas));
-        exibirDados(); // atualiza a tabela após a exclusão
-      });
+  const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
+  pessoas.forEach((pessoa, index) => {
+    const tr = document.createElement('tr');
+    const tdNome = document.createElement('td');
+    const tdAniversario = document.createElement('td');
+    const tdBotoes = document.createElement('td');
 
-    tdExcluir.appendChild(btnExcluir); // adiciona o botão à nova coluna
+    tdNome.textContent = pessoa.nome;
+    tdAniversario.textContent = pessoa   .birthdate;
+
+    const btnExcluir = document.createElement('button');
+    btnExcluir.textContent = 'Excluir';
+    btnExcluir.addEventListener('click', () => {
+      pessoas.splice(index, 1);
+      localStorage.setItem('pessoas', JSON.stringify(pessoas));
+      exibirDados();
+    });
+
+    const btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.addEventListener('click', () => {
+      editIndex = index;
+      fillFormWithPersonData(pessoa);
+    });
+
+    tdBotoes.appendChild(btnEditar);
+    tdBotoes.appendChild(btnExcluir);
     tr.appendChild(tdNome);
-    tr.appendChild(tdAniversario);  
-    tr.appendChild(tdExcluir); // adiciona a nova coluna à linha
+    tr.appendChild(tdAniversario);
+    tr.appendChild(tdBotoes);
     tbody.appendChild(tr);
   });
 }
-// função para lidar com o envio do formulário
+
+let editIndex = -1;
+
+function fillFormWithPersonData(person) {
+  const form = document.querySelector('.js-form');
+  form.elements.name.value = person.nome;
+  form.elements.birthdate.value = person.birthdate;
+}
+
 function handleFormSubmit(event) {
-  // impede o comportamento padrão do formulário
   event.preventDefault();
 
-  // coleta os valores do formulário
   const form = event.target;
   const nome = form.elements.name.value;
-  const aniversario = form.elements.birthdate.value;
+  const birthdate = form.elements.birthdate.value;
 
-  // cria um objeto pessoa com os valores coletados
-  const pessoa = { nome, aniversario };
+  const pessoa = { nome, birthdate };
 
-  // recupera os dados salvos localmente e adiciona a nova pessoa
-  const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
-  pessoas.push(pessoa);
+  let pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
 
-  // salva os dados localmente
+  if (editIndex === -1) {
+    pessoas.push(pessoa);
+  } else {
+    pessoas[editIndex] = pessoa;
+    editIndex = -1;
+  }
+
   localStorage.setItem('pessoas', JSON.stringify(pessoas));
 
-  // exibe os dados salvos em uma tabela
   exibirDados();
 
-  // reseta o formulário
   form.reset();
-};
+} 
+
+// função para lidar com o envio do formulário
 
 // adiciona um ouvinte de eventos para o envio do formulário
 const form = document.querySelector('.js-form');
@@ -91,5 +104,3 @@ form.addEventListener('submit', handleFormSubmit);
 
 // exibe os dados salvos em uma tabela quando a página é carregada
 document.addEventListener('DOMContentLoaded', exibirDados);
-
-

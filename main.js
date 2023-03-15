ca37c521a4c7fde47721ae77c0f2492ead584c76
thamires <thamiresamaral2015@gmail.com>
2023-03-15 20:10:31 -0300

    estlizando a página

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
+function checkValidity() {
+  let nameField = document.querySelector('#name');
+  let nameError = document.querySelector('#name + .error-message');
+  let birthField = document.querySelector('birthdate');
 
-function checkValidity(){
-let nameField = document.querySelector('#name');
-let birthdate = document.querySelector('#birthdate');
-// Adiciona um ouvinte de evento de entrada ao campo de entrada de nome
-nameField.addEventListener('input', () => {
-  nameField.setCustomValidity(""); // remove qualquer mensagem de erro personalizada existente
-  const name = nameField.value; 
-  const nameRegex = /^[a-zA-Z\s]*$/; 
-
-  // Verifica se o valor do campo de entrada de nome não corresponde à expressão regular
-  if (!nameRegex.test(name)){
-    nameField.setCustomValidity("Insira apenas letras e espaços"); // define uma mensagem de erro personalizada
-  }
-});
-
-nameField.addEventListener('change', () => {
-  if (nameField.checkValidity()) {
-    nameField.setCustomValidity(""); // remove qualquer mensagem de erro personalizada existente se o valor do campo for válido
-  }
-});
- 
+  nameField.addEventListener('input', () => {
+    nameField.setCustomValidity(""); // remove qualquer mensagem de erro personalizada existente
+    const name = nameField.value; 
+    const nameRegex = /^[A-Za-z ]*$/; // permite apenas letras e espaços em branco
+    
+    if (!nameRegex.test(name)){
+      nameField.setCustomValidity("Insira apenas letras e espaços"); // define uma mensagem de erro personalizada
+      nameError.textContent = "Insira apenas letras e espaços"; // exibe a mensagem de erro personalizada
+      nameError.style.display = 'block'; // exibe a mensagem de erro
+    } else {
+      nameError.style.display = 'none'; // oculta a mensagem de erro
+    }
+  });
 }
 
 
@@ -37,15 +33,27 @@ function exibirDados() {
   pessoas.forEach((pessoa, index) => {
     const tr = document.createElement('tr');
     const tdNome = document.createElement('td');
+    tdNome.classList.add('tabela-nome')
     const tdAniversario = document.createElement('td');
+    tdAniversario.classList.add('tabela-aniversario')
     const tdBotoes = document.createElement('td');
+
 
     // Adiciona os valores dos atributos 'nome' e 'birthdate' da pessoa nas células correspondentes
     tdNome.textContent = pessoa.nome;
     tdAniversario.textContent = pessoa.birthdate;
+     
+      // Formata a data de aniversário no formato DD/MM/AAAA
+      const dataAniversario = new Date(pessoa.birthdate);
+      const dia = dataAniversario.getDate().toString().padStart(2, '0');
+      const mes = (dataAniversario.getMonth() + 1).toString().padStart(2, '0');
+      const ano = dataAniversario.getFullYear().toString();
+      tdAniversario.textContent = `${dia}/${mes}/${ano}`;
+  
 
     // Cria o botão de excluir e adiciona um listener para removê-la do array de pessoas e do localStorage
     const btnExcluir = document.createElement('button');
+    btnExcluir.classList.add('botao', 'botao-excluir')
     btnExcluir.textContent = 'Excluir';
     btnExcluir.addEventListener('click', () => {
       pessoas.splice(index, 1);
@@ -55,6 +63,7 @@ function exibirDados() {
 
     // Cria o botão de editar e adiciona um listener para preencher o formulário com os dados da pessoa selecionada
     const btnEditar = document.createElement('button');
+    btnEditar.classList.add('botao', 'botao-editar')
     btnEditar.textContent = 'Editar';
     btnEditar.addEventListener('click', () => {
       editIndex = index;
@@ -73,13 +82,16 @@ function exibirDados() {
   });
 }
 
+
 let editIndex = -1; // variável que guarda o índice da pessoa que será editada, inicialmente é -1
+
 
 function fillFormWithPersonData(person) {
   const form = document.querySelector('.js-form');
   form.elements.name.value = person.nome; // preenche o campo de nome do formulário com o nome da pessoa que será editada
   form.elements.birthdate.value = person.birthdate; // preenche o campo de data de nascimento do formulário com a data de nascimento da pessoa que será editada
 }
+
   // função para lidar com o envio do formulário
 function handleFormSubmit(event) {
   event.preventDefault(); 
@@ -87,6 +99,7 @@ function handleFormSubmit(event) {
   const form = event.target; // obtém o formulário que foi submetido
   const nome = form.elements.name.value; // obtém o valor do campo de nome do formulário
   const birthdate = form.elements.birthdate.value; // obtém o valor do campo de data de nascimento do formulário
+
 
   const pessoa = { nome, birthdate };
 
@@ -100,12 +113,13 @@ function handleFormSubmit(event) {
   }
 
   localStorage.setItem('pessoas', JSON.stringify(pessoas)); // salva as pessoas no localStorage em formato JSON
-
+  
   exibirDados(); 
 
   form.reset(); // reseta o formulário para que possa ser usado novamente
 }
 
+checkValidity();
 
 // adiciona um ouvinte de eventos para o envio do formulário
 const form = document.querySelector('.js-form');
